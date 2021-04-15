0c7069f320f5b0c72784a7af54492c56cda5f22c
Eduardo Rossel <eduardorossel@outlook.cl>
2021-04-14 21:18:24 -0400

    added category scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_010811) do
+ActiveRecord::Schema.define(version: 2021_04_15_011734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2021_04_13_010811) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_categories_on_category_id"
   end
 
   create_table "news", force: :cascade do |t|
@@ -55,5 +63,6 @@ ActiveRecord::Schema.define(version: 2021_04_13_010811) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "categories"
   add_foreign_key "news", "admins"
 end
