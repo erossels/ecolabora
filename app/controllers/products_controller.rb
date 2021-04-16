414d56dfc44aca1dff74b2fb43500f7a831dc9e1
Eduardo Rossel <eduardorossel@outlook.cl>
2021-04-16 10:41:51 -0400

    modified devise controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,6 +64,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:category_id, :user_id, :description, :action, :status)
+      params.require(:product).permit(:category_id, :user_id, :description, :action, :status, photos: [])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,12 @@ Rails.application.routes.draw do
   resources :categories
   resources :news
   devise_for :admins
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   get 'home/index'
   root 'home#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
