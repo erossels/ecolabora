28c3dfd0504c6da2ba6fed8f58c5d82d97085e05
Eduardo Rossel <eduardorossel@outlook.cl>
2021-04-26 10:47:57 -0400

    fixing migrations for heroku

--- db/migrate/20210415015254_create_alerts.rb
+++ db/migrate/20210415015254_create_alerts.rb
@@ -2,7 +2,6 @@ class CreateAlerts < ActiveRecord::Migration[6.0]
   def change
     create_table :alerts do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
