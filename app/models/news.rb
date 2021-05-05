5133e1258f10022c27c5f8758d31b7bedbdda852
Eduardo Rossel <eduardorossel@outlook.cl>
2021-05-04 23:02:49 -0400

    fixed seeds for news 2

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,7 +2,7 @@ class News < ApplicationRecord
   belongs_to :admin
   has_one_attached :photo
 
-  validates :name, presence: { message: "La noticia debe llevar un título" }
+  validates :title, presence: { message: "La noticia debe llevar un título" }
   validates :content, presence: { message: "El contenido no puede estar vacío" }
   after_validation :report_validation_errors_to_rollbar
 
