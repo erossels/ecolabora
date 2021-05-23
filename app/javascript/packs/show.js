a2f8bbdcec2d445c50805d4d4588e4dcb78b139e
Eduardo Rossel <eduardorossel@outlook.cl>
2021-05-23 11:20:01 -0400

    fix javascript error

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,17 +1,11 @@
 document.addEventListener("turbolinks:load", function() {
   $('.btn-modal').on('click', function(){
-    $('#PhotosModal').modal('show') 
+    $('#PhotosModal').modal('show');
   })  
 
   $('.btn-close-modal').on('click',function(){
-    $('.modal').modal('hide')
+    $('.modal').modal('hide');
   })
-
-  $(".fa-paper-plane").on('click', function(){
-    $(".fa-paper-plane").innerHTML = ''
-    $(".msg_card_body").innerHTML = ''
-    $(".msg_card_body").append("<%= escape_javascript(render partial: 'chats/show', locals: { chat: @chat } ) %>")
-  }
 
 });
 
