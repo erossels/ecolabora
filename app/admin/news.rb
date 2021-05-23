367080731248c0511b05a8471c0f818e4dc12a8c
Eduardo Rossel <eduardorossel@outlook.cl>
2021-05-23 17:26:39 -0400

    modifications to the news section

---
+++ app/admin/news.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register User do
+
+  permit_params do
+    permitted = [:title, :content, :created_at, :admin_id]
+    permitted << :other if params[:action] == 'create' && current_user.admin?
+    permitted
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :content
+    column :created_at
+    column :admin_id
+    actions
+  end
+
+  filter :title
+  filter :content
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :content
+      f.input :photo
+    end
+    f.actions
+  end
+  
+end
