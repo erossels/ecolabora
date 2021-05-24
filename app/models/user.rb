9735f7975e69a155c6838a2f4a655846e2787249
Eduardo Rossel <eduardorossel@outlook.cl>
2021-05-24 19:42:57 -0400

    change production.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
     first_name
   end
 
-  #omniauth Facebook
+  # omniauth Facebook
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
