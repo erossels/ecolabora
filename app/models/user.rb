08ca52e41af4e46cd3fe02cf830ecf742135c365
Eduardo Rossel <eduardorossel@outlook.cl>
2021-05-25 23:04:51 -0400

    omniauth wout avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,6 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.avatar = auth.info.image
       parse_name(user, auth.info.name)
     end
   end
